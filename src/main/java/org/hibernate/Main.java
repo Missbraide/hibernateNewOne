ace1e90a5862803f1f840679af8bf66f470c2663
Ella Braide <88412294+Missbraide@users.noreply.github.com>
2022-05-12 15:52:22 -0700

    updated  main, files and pom

--- src/main/java/org/hibernate/Main.java
+++ src/main/java/org/hibernate/Main.java
@@ -1,13 +1,84 @@
 package org.hibernate;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+import org.hibernate.models.*;
+import org.hibernate.services.EmployeeService;
+import org.hibernate.services.ItemService;
+import org.hibernate.services.ProjectService;
+import org.hibernate.util.HibernateUtil;
+import lombok.extern.java.Log;
+import org.hibernate.Session;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Log
+public class Main{
+    public static void main(String[] args) throws InterruptedException {
+        EmployeeService employeeService = new EmployeeService();
+        ProjectService projectService = new ProjectService();
+        ItemService itemService = new ItemService();
+
+        Employee e1 = new Employee("jafer", 70000);
+        Address a1 = new Address("123 st", "", "plano", 75000, e1);
+
+        employeeService.createEmployee(new Employee("john", 60000));
+        employeeService.createEmployee(new Employee("jane", 50000));
+
+        boolean saved = employeeService.createEmployeeAndAddress(a1);
+
+        // e1.setId(233);
+        e1.setName("someone else");
+        e1.setSalary(101010);
+        boolean updated = employeeService.updateEmployee(e1);
+        log.info("Saved: "+Boolean.toString(saved));
+        log.info("Updated: "+Boolean.toString(updated));
+        log.info(employeeService.getAllEmployees().toString());
+        log.info(employeeService.getEmployeeById(1).toString());
+        log.info(employeeService.findEmployeeSalaryGreaterThan(100000).toString());
+        log.info(Boolean.toString(employeeService.deleteEmployee(employeeService.getEmployeeById(1))));
+        log.info(employeeService.findEmployeeAddresses(e1).toString());
+        log.info(employeeService.EmployeeIdAndName().toString());
+        Address a5 = new Address("555 st", "", "seattle", 98109, new Employee());
+        employeeService.addAddress(a5,3);
+        log.info(employeeService.findEmployeeAddresses(e1).toString());
+        Address a6 = new Address("252 st", "", "seattle", 98109, new Employee());
+        employeeService.addAddress(a6,2);
+        Employee someone = new Employee("",12);
+        someone.setId(2);
+        log.info(employeeService.findEmployeeAddresses(someone).toString());
+
+        Item i = new Item("Cookies", new ItemDescription(10,10,30));
+        itemService.createItem(i);
+
+
+        Project p1 = new Project("Hibernate");
+        Project p2 = new Project("Java");
+        Project p3 = new Project("Spring Boot");
+        projectService.createProject(p1);
+        projectService.createProject(p2);
+        projectService.createProject(p3);
+
+        projectService.addEmployeeToProject(p1.getId(),2);
+        projectService.addEmployeeToProject(p1.getId(),3);
+        projectService.addEmployeeToProject(p2.getId(),3);
+
+        log.info("Native:" + projectService.getAllEmployeesInProject(p2).toString());
+
+        log.info(projectService.getAllEmployeesInProjectHql(p2).toString());
+        // in case using h2 db sleep the application to access db data
+        // Thread.sleep(10000);
+
+        Employee employeeJafer = new Employee("jafer", 75555);
+        Address addressJafer = new Address("545585 st", "apt 1234", "plano", 75000);
+        Address addressJaferTwo = new Address("4555444 st", "apt 6565", "dallas", 76000);
+
+        employeeJafer.addAddress(addressJafer);
+        employeeJafer.addAddress(addressJaferTwo);
+        employeeService.createEmployee(employeeJafer);
+
+
+
+
+
+    } // main method
+} // MainRunner
--- src/main/java/org/hibernate/dao/EmployeeI.java
+++ src/main/java/org/hibernate/dao/EmployeeI.java
@@ -3,7 +3,9 @@ package org.hibernate.dao;
 import org.hibernate.models.Address;
 import org.hibernate.models.Employee;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public interface EmployeeI {
     List<Employee> getAllEmployees();
@@ -14,4 +16,6 @@ public interface EmployeeI {
     Employee getEmployeeById(int id);
     List<Employee> findEmployeeSalaryGreaterThan(double salary);
     List<Address> findEmployeeAddresses(Employee e);
+    Map<Integer, String> EmployeeIdAndName();
+    void addAddress(Address a, int emp_id);
 }
